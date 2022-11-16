ce4d4bdc8a27072ee92c3294a5ae85be582c8a80
Bryan G-F <bryan@wearestateside.com>
2022-11-16 11:59:51 -0600

    Quick update to get .gitignore setup properly

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,27 +1,39 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
-import { VStack, Flex, Heading } from "@chakra-ui/layout"
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Button } from '@chakra-ui/react'
-import WalletIcon from '@/components/icons/WalletIcon'
+import { Flex, Heading, Box } from "@chakra-ui/layout"
+import { Alert, AlertDescription, AlertIcon, AlertTitle } from '@chakra-ui/react'
 import Connect from '@/components/Connect'
 import { homeLabels, getLabel } from '@/store/initial'
 import { useWeb3React } from "@web3-react/core";
+import { ethers } from 'ethers'
 
 const Home: NextPage = () => {
   const [error, setError] = useState<string>("")
-  const { active } = useWeb3React();
+  const { active, account } = useWeb3React();
+  // let etherscanProvider = new ethers.providers.EtherscanProvider();
+
+  // console.log('TESTING', etherscanProvider.getHistory(account))
 
   return (
-    <VStack width='100%' >
+    <Box width='100%' >
       <Head>
         <title>{homeLabels.pageTitle}</title>
       </Head>
-
+      {
+        // Testing code ove here.
+        // console.log('WEB3 ON YEAH', account, etherscanProvider)
+      }
+      {/* {
+        etherscanProvider.getHistory(account).then((history) => {
+          history.forEach((tx) => {
+              console.log(tx);
+          })
+        })
+      } */}
       <Flex
         width='100%'
         margin={0}
-        maxWidth='1140px'
         flexDirection='column'
       >
         {error &&
@@ -39,14 +51,15 @@ const Home: NextPage = () => {
           marginBottom='48px'>
           {getLabel(active, homeLabels.mainTitle)}
         </Heading>
+
         {!active &&
           <Connect 
             variant='Heading'
             w='250px'
-            ctaText={homeLabels.ctaConnect} />
+            label={homeLabels.ctaConnect} />
         }
       </Flex>
-    </VStack>
+    </Box>
   )
 }
 
