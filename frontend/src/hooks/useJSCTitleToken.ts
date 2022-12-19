0722d3f26f256341e39c7dcb00546536de59f96b
Willmer Barahona <wbarahona@live.com>
2022-12-19 20:56:22 +0100

    [JHB-17] connected to the data through useJSCTitleToken hook added token details factory

--- frontend/src/hooks/useJSCTitleToken.ts
+++ frontend/src/hooks/useJSCTitleToken.ts
@@ -5,11 +5,11 @@ import { ethers } from 'ethers';
 import { Token, Offer } from '@/interfaces/index';
 import * as tc from '../../typechain-types';
 
-type UseJSCTitleTOkenHook = [Token[], boolean, string];
+type useJSCTitleTOkenHook = [Token[], boolean, string];
 
 const useJSCTitleToken = (
   jscTitleTokenConnect: string
-): UseJSCTitleTOkenHook => {
+): useJSCTitleTOkenHook => {
   const [error, setError] = useState<string>('');
   const [loading, setLoading] = useState(true);
   const { active, account, library } = useWeb3React();
@@ -18,6 +18,19 @@ const useJSCTitleToken = (
   >(undefined);
   const [tokens, setTokens] = useState<Token[]>([]);
 
+  useEffect(() => {
+    if (active && account && library)
+      try {
+        setJSCTitleToken(
+          tc.IJSCTitleToken__factory.connect(
+            jscTitleTokenConnect as string,
+            library
+          )
+        );
+      } catch (err) {
+        setError(err as string);
+      }
+  }, [active, account]);
   useEffect(() => {
     if (active && account && library)
       try {
@@ -41,7 +54,26 @@ const useJSCTitleToken = (
           const t = await jscTitleToken.tokenAtIndex(ti);
           const { owner, titleId, offersToBuy, offersToSell, frozen, url } =
             await getTokenData(jscTitleToken, t);
+  useEffect(() => {
+    if (jscTitleToken) {
+      const loadData = async () => {
+        let _tokens: Token[] = [];
+        const tokensCounter = await getTotalTokens(jscTitleToken);
+        for (let ti = 0; ti < tokensCounter; ti++) {
+          const t = await jscTitleToken.tokenAtIndex(ti);
+          const { owner, titleId, offersToBuy, offersToSell, frozen, url } =
+            await getTokenData(jscTitleToken, t);
 
+          _tokens.push({
+            tokenId: t.toHexString(),
+            titleId,
+            frozen,
+            owner: accountsByAddress[owner.toLowerCase()].name,
+            ownerFrozen: await jscTitleToken.isFrozenOwner(owner),
+            url,
+            offersToBuy,
+            offersToSell,
+          });
           _tokens.push({
             tokenId: t.toHexString(),
             titleId,
@@ -63,11 +95,37 @@ const useJSCTitleToken = (
       });
     }
   }, [jscTitleToken]);
+        setTokens(_tokens);
+        setLoading(false);
+      };
+      loadData().catch(err => {
+        console.log(err);
+        setError(err.toString());
+        setLoading(false);
+      });
+    }
+  }, [jscTitleToken]);
 
+  return [tokens, loading, error];
   return [tokens, loading, error];
 };
 
 const getOffersToBuy = async (jscTitleToken: any, token: any) => {
+  const offersToBuy: Offer[] = [];
+  const offersToBuyCount = (
+    await jscTitleToken.countOffersToBuy(token)
+  ).toNumber();
+  for (let obi = 0; obi < offersToBuyCount; obi++) {
+    const ob = await jscTitleToken.offerToBuyAtIndex(token, obi);
+    const accountInfo = accountsByAddress[ob.buyer.toLowerCase()];
+    offersToBuy.push({
+      amount: parseFloat(ethers.utils.formatEther(ob.amount)),
+      buyer: accountInfo.name,
+      buyerAddress: accountInfo.address,
+    });
+  }
+  return offersToBuy;
+};
   const offersToBuy: Offer[] = [];
   const offersToBuyCount = (
     await jscTitleToken.countOffersToBuy(token)
@@ -98,8 +156,23 @@ const getOffersToSell = async (jscTitleToken: any, token: any) => {
   }
   return offersToSell;
 };
+  const offersToSell: Offer[] = [];
+  const osCount = (await jscTitleToken.countOffersToSell(token)).toNumber();
+  for (let osi = 0; osi < osCount; osi++) {
+    const os = await jscTitleToken.offerToSellAtIndex(token, osi);
+    const accountInfo = accountsByAddress[os.buyer.toLowerCase()];
+    offersToSell.push({
+      amount: parseFloat(ethers.utils.formatEther(os.amount)),
+      buyer: accountInfo.name,
+      buyerAddress: accountInfo.address,
+    });
+  }
+  return offersToSell;
+};
 
 const getTotalTokens = async (jscTitleToken: any) => {
+  return (await jscTitleToken.totalSupply()).toNumber() || 0;
+};
   return (await jscTitleToken.totalSupply()).toNumber() || 0;
 };
 
@@ -119,5 +192,21 @@ const getTokenData = async (jscTitleToken: any, token: any) => {
     frozen: frozen,
   };
 };
+  const owner = await jscTitleToken.ownerOf(token);
+  const url = ''; //await jscTitleToken.tokenURI(t)
+  const titleId = await jscTitleToken.tokenToTitleId(token);
+  const offersToBuy = await getOffersToBuy(jscTitleToken, token);
+  const offersToSell = await getOffersToSell(jscTitleToken, token);
+  const frozen = await jscTitleToken.isFrozenToken(token);
+  return {
+    owner: owner,
+    url: url,
+    titleId: titleId,
+    offersToBuy: offersToBuy,
+    offersToSell: offersToSell,
+    frozen: frozen,
+  };
+};
 
 export default useJSCTitleToken;
+
