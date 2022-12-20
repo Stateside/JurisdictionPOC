63752d9df9e6e8fa4b1bd6b3485b61e72746e77d
Luis Cubillo <luis.cubillo.soto@gmail.com>
2022-12-20 17:07:34 -0600

    Displays proposal name

--- frontend/src/pages/jurisdiction/Proposals.tsx
+++ frontend/src/pages/jurisdiction/Proposals.tsx
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { ProposalState } from '@/utils/types';
+import { PreparedProposal } from '@/utils/proposals';
+import { createSampleProposals } from '@/utils/sample-proposals';
 import * as tc from '../../../typechain-types';
 import { Box, Button, HStack, Select, Text, VStack } from '@chakra-ui/react';
 import Tag from '@/components/Tag';
 
+type PreparedProposalMap = { [hash: string]: PreparedProposal };
 type BlockchainProposal = { id: string; state: ProposalState };
 type BlockchainProposalMap = { [hash: string]: BlockchainProposal };
 
 const Proposals = () => {
   const [localError, setLocalError] = useState<string>('');
   const { active, account, library, error } = useWeb3React();
+  const [jscJurisdiction, setJSCJurisdiction] = useState<
+    tc.IJSCJurisdiction | undefined
+  >(undefined);
   const [jscGovernor, setJSCGovernor] = useState<tc.IJSCGovernor | undefined>(
     undefined
   );
+  const [sampleProposals, setSampleProposals] = useState<PreparedProposalMap>(
+    {}
+  );
+  const [jscCabinet, setJSCCabinet] = useState<tc.IJSCCabinet | undefined>(
+    undefined
+  );
+  const [jscTitleToken, setJSCTitleToken] = useState<
+    tc.IJSCTitleToken | undefined
+  >(undefined);
   const [jscProposals, setJSCProposals] = useState<BlockchainProposalMap>({});
 
   useEffect(() => {
@@ -29,6 +44,24 @@ const Proposals = () => {
         setLocalError(err as string);
       }
   }, [library]);
+
+  useEffect(() => {
+    const getJurisdictionAddress = async () => {
+      try {
+        if (jscGovernor)
+          setJSCJurisdiction(
+            tc.IJSCJurisdiction__factory.connect(
+              await jscGovernor.getJurisdiction(),
+              library
+            )
+          );
+      } catch (err) {
+        setLocalError(err as string);
+      }
+    };
+    if (library && jscGovernor)
+      getJurisdictionAddress().catch(err => setLocalError(err.toString()));
+  }, [library, jscGovernor]);
 
   useEffect(() => {
     if (jscGovernor) {
@@ -48,15 +81,64 @@ const Proposals = () => {
     }
   }, [jscGovernor]);
 
+  useEffect(() => {
+    const loadSampleProposals = async () => {
+      if (jscGovernor && jscCabinet && jscTitleToken) {
+        const sp = await createSampleProposals(
+          jscGovernor,
+          jscCabinet,
+          jscTitleToken
+        );
+        const proposalMap: { [hash: string]: PreparedProposal } = {};
+        for (let i = 0; i < sp.length; i++) {
+          const p = sp[i];
+          proposalMap[p.proposalHash.toHexString().toLowerCase()] = p;
+        }
+        setSampleProposals(proposalMap);
+      }
+    };
+    loadSampleProposals().catch(err => setLocalError(err.toString()));
+  }, [jscGovernor, jscCabinet, jscTitleToken]);
+
+  useEffect(() => {
+    const getAddresses = async () => {
+      if (library && jscJurisdiction)
+        try {
+          setJSCCabinet(
+            tc.IJSCCabinet__factory.connect(
+              (await jscJurisdiction.getContractAddress(
+                'jsc.contracts.cabinet'
+              )) as string,
+              library
+            )
+          );
+          setJSCTitleToken(
+            tc.IJSCTitleToken__factory.connect(
+              (await jscJurisdiction.getContractAddress(
+                'jsc.contracts.tokens'
+              )) as string,
+              library
+            )
+          );
+        } catch (err) {
+          setLocalError((err as any).toString());
+        }
+    };
+    getAddresses().catch(err => setLocalError(err.toString()));
+  }, [library, jscJurisdiction]);
+
   return (
     <HStack alignItems="flex-start">
       <VStack width={'80%'} marginRight={'10%'} alignItems="flex-start">
         <Text variant={'15/20-BOLD'}>Active proposals</Text>
         <Box width={'100%'}>
           {Object.keys(jscProposals).map(proposal => {
+            const cProposal = jscProposals[proposal];
             return (
-              <Tag>
-                <Text variant={'15/20'}>{proposal}</Text>
+              !!cProposal.state && <Tag>
+                <Text variant={'15/20'}>
+                  {sampleProposals[proposal]?.description || 'unknown'}
+                </Text>
               </Tag>
             );
           })}
@@ -64,10 +146,15 @@ const Proposals = () => {
         <Text variant={'15/20-BOLD'}>Closed proposals</Text>
         <Box width={'100%'}>
           {Object.keys(jscProposals).map(proposal => {
+            const cProposal = jscProposals[proposal];
             return (
-              <Tag>
-                <Text variant={'15/20'}>{proposal}</Text>
-              </Tag>
+              !cProposal.state && (
+                <Tag>
+                  <Text variant={'15/20'}>
+                    {sampleProposals[proposal]?.description || 'unknown'}
+                  </Text>
+                </Tag>
+              )
             );
           })}
         </Box>
