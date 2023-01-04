98bfbdfef26d3e21da6b3c4a47339841763a2fa8
Luis Cubillo <luis.cubillo.soto@gmail.com>
2023-01-04 17:17:55 -0600

    Fixes on properties UI

--- frontend/src/pages/my-properties/Property.tsx
+++ frontend/src/pages/my-properties/Property.tsx
@@ -7,11 +7,11 @@ const Property = () => {
     <Tag>
       <HStack width="100%">
         {/* Name */}
-        <Text style={{ flexGrow: 1 }} variant={'15/20'}>
+        <Text variant={'15/20'}>
           001 - 456 - 87654 - E
         </Text>
         {/* Activity */}
-        <Text style={{ flexGrow: 1 }} variant={'15/20'}>
+        <Text style={{ position: "absolute", right: "50px" }} variant={'15/20'}>
           <RealStateAgentIcon height={5} width={5} /> Selling for <b>78 ETH</b>
         </Text>
       </HStack>
