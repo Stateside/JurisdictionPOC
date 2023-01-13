1c6960d27f7e481806bdee4b225f678b48a83c15
Luis Cubillo <luis.cubillo.soto@gmail.com>
2023-01-12 19:02:41 -0600

    Fixes on proposals tab

--- frontend/src/components/Tag.tsx
+++ frontend/src/components/Tag.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { ObjectHashInterface, TagInterface } from '@/interfaces/index';
 import ChevronRightIcon from '@/components/icons/chevronRightIcon';
 
@@ -6,6 +6,7 @@ export default function Tag({
   type = 'default',
   caret,
   justify,
+  information,
   ...props
 }: TagInterface) {
   const tags: ObjectHashInterface = {
@@ -20,16 +21,17 @@ export default function Tag({
       _hover={{
         // padding: '6px 8px 8px 8px',
         // border: '3px solid',
-        boxShadow: 'inset 0 0 0 3px var(--chakra-colors-brand-javaHover), 0px 0px 15px rgba(0, 0, 0, 0.1)',
+        boxShadow:
+          'inset 0 0 0 3px var(--chakra-colors-brand-javaHover), 0px 0px 15px rgba(0, 0, 0, 0.1)',
         cursor: 'pointer',
         borderColor: type === 'sellingMe' ? 'brand.grey04' : 'brand.javaHover',
-        backgroundColor: type === 'sellingMe' ? 'brand.lemon' : 'brand.white'
+        backgroundColor: type === 'sellingMe' ? 'brand.lemon' : 'brand.white',
       }}
       alignItems="center"
       boxShadow="0px 0px 15px rgba(0, 0, 0, 0.1)"
       borderRadius="8px"
       position="relative"
-      justify={justify||'flex-start'}
+      justify={justify || 'flex-start'}
       direction="row"
       background={tags[type]}
       h={'40px'}
@@ -44,6 +46,21 @@ export default function Tag({
       {...props}
     >
       {props.children}
+      {information && (
+        <Box
+          position="absolute"
+          right={10}
+          fontSize={12}
+          pt={0.5}
+          pb={0.5}
+          pr={1}
+          pl={1}
+          borderRadius={10}
+          backgroundColor="#D3D3D3"
+        >
+          Status
+        </Box>
+      )}
       <Flex position="absolute" right="2">
         {caret === null || <ChevronRightIcon />}
       </Flex>
